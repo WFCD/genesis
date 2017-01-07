3060fb75c879395b7993c511ffef7c3be7c3296e
Matej Voboril <matej@voboril.org>
2017-01-07 00:52:58 -0600

    add pricecheck and whereis

---
+++ src/commands/Pricecheck.js
@@ -0,0 +1,85 @@
+'use strict';
+
+const Nexus = require('warframe-nexus-query');
+const Command = require('../Command.js');
+
+
+/**
+ * Looks up items from Nexus-stats.com
+ */
+class PriceCheck extends Command {
+  /**
+   * Constructs a callable command
+   * @param {Genesis} bot  The bot object
+   */
+  constructor(bot) {
+    super(bot, 'misc.pricecheck', 'pricecheck', 'pricecheck');
+    this.regex = new RegExp(`^${this.bot.escapedPrefix}p(?:rice)?\\s?c(?:heck)?(?:\\s+([\\w+\\s]+))?`, 'i');
+
+    this.usages = [
+      {
+        description: 'Display an items worth from nexus-stats.com',
+        parameters: ['item'],
+      },
+    ];
+
+    this.nexusQuerier = new Nexus();
+  }
+
+  /**
+   * Run the command
+   * @param {Message} message Message with a command to handle, reply to,
+   *                          or perform an action based on parameters.
+   */
+  run(message) {
+    const item = message.content.match(this.regex)[1];
+    this.nexusQuerier.priceCheckQueryAttachment(item)
+      .then((result) => {
+        if (typeof result[0] === 'string') {
+          const embed = {
+            color: 0xff0000,
+            author: {
+              name: this.bot.client.user.username,
+              icon_url: this.bot.client.user.avatarURL,
+            },
+            title: 'Warframe - Pricecheck',
+            url: 'http://nexus-stats.com',
+            description: `Pricecheck for: ${item}`,
+            thumbnail: {
+              url: 'https://cdn.discordapp.com/icons/195582152849620992/4c1fbd47b3e6c8d49b6d2362c79a537b.jpg',
+            },
+            fields: [
+              {
+                name: '_ _',
+                value: result[0],
+                inline: true,
+              },
+            ],
+            footer: {
+              icon_url: 'https://cdn.discordapp.com/icons/195582152849620992/4c1fbd47b3e6c8d49b6d2362c79a537b.jpg',
+              text: 'Pricechecks provided by Nexus Stats - https://nexus-stats.com',
+            },
+          };
+          message.channel.sendMessage('', { embed }).catch(this.logger.error);
+        } else {
+          const attachment = result[0];
+          const embed = {
+            type: attachment.type,
+            author: {
+              name: this.bot.client.user.username,
+              icon_url: this.bot.client.user.avatarURL,
+            },
+            title: attachment.title,
+            url: attachment.url,
+            fields: attachment.fields,
+            thumbnail: attachment.thumbnail,
+            footer: attachment.footer,
+          };
+          message.channel.sendMessage('', { embed }).catch(this.logger.error);
+        }
+      })
+      .catch(this.bot.logger.error);
+  }
+}
+
+module.exports = PriceCheck;
---
+++ src/commands/WhereIs.js
@@ -0,0 +1,74 @@
+'use strict';
+
+const Query = require('warframe-location-query');
+const Command = require('../Command.js');
+
+const extraSpace = '　　';
+
+/**
+ * Looks up items from Nexus-stats.com
+ */
+class PriceCheck extends Command {
+  /**
+   * Constructs a callable command
+   * @param {Genesis} bot  The bot object
+   */
+  constructor(bot) {
+    super(bot, 'misc.pricecheck', 'pricecheck', 'pricecheck');
+    this.regex = new RegExp(`^${this.bot.escapedPrefix}where(?:\\s?is)?(?:\\s+([\\w+\\s]+))?`, 'i');
+
+    this.usages = [
+      {
+        description: 'Display an items worth from nexus-stats.com',
+        parameters: ['item'],
+      },
+    ];
+
+    this.querier = new Query();
+  }
+
+  /**
+   * Run the command
+   * @param {Message} message Message with a command to handle, reply to,
+   *                          or perform an action based on parameters.
+   */
+  run(message) {
+    const item = message.content.match(this.regex)[1];
+    this.querier.getAll(item)
+      .then((results) => {
+        const color = Object.prototype.toString.call(results) === '[object Array]' ? 0x00ff00 : 0xff0000;
+        const fields = Object.prototype.toString.call(results) !== '[object Array]'
+          ? [{ name: '_ _', value: 'Operator, there is no such item location available.' }] : [];
+
+        if (Object.prototype.toString.call(results) === '[object Array]') {
+          results.forEach((result) => {
+            fields.push({
+              name: result.component + (result.type === 'Prime Part' ?
+                ` worth ${result.ducats}` : ''),
+              value: result.locations.join(`,${this.bot.md.lineEnd}${extraSpace}`),
+            });
+          });
+        }
+        const embed = {
+          color,
+          author: {
+            name: this.bot.client.user.username,
+            icon_url: this.bot.client.user.avatarURL,
+          },
+          title: 'Warframe Where Is?',
+          url: 'https://warframe.com',
+          description: '\n\n',
+          fields,
+          footer: {
+            icon_url: 'https://avatars1.githubusercontent.com/u/24436369',
+            text: 'Data evaluated by warframe-location-query, Warframe Community Developers',
+          },
+        };
+        console.log(results);
+        message.channel.sendMessage('', { embed });
+      })
+      .catch(this.logger.error);
+  }
+}
+
+module.exports = PriceCheck;
