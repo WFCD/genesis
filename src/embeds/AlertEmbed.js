a02adbe5482f2edf90f4684a0083f53f2eda1c37
Matej Voboril <matej@voboril.org>
2017-01-18 00:29:59 -0600

    a bit better alert formatting, invasion singleton

--- src/embeds/AlertEmbed.js
+++ src/embeds/AlertEmbed.js
@@ -2,6 +2,7 @@
 
 const BaseEmbed = require('./BaseEmbed.js');
 
+const extraSpace = '　';
 /**
  * Generates alert embeds
  */
@@ -24,16 +25,20 @@ class AlertEmbed extends BaseEmbed {
       this.description = 'Currently in-progress alerts:';
     } else {
       const a = alerts[0];
-      this.title = a.getReward().toString().replace(/\s*\+\s*\d*cr/ig, '').replace(/^1\s/, '');
+      const item = a.getReward().toString().replace(/\s*\+\s*\d*cr/ig, '').replace(/^1\s/, '');
+      this.title = item === '' ? a.getReward().replace(/^1\s/, '') : item;
       this.color = 0x00ff00;
-      this.description = a.getDescription() && a.getDescription() !== '' ? a.getDescription() : '　';
-      this.fields = [
-        { name: 'Time Remaining', value: a.getETAString(), inline: true },
-        { name: 'Credits', value: `${a.getReward().credits}cr`, inline: true },
-        { name: 'Mission', value: `${a.mission.faction} ${a.mission.type}`, inline: true },
-        { name: 'Location', value: a.mission.node, inline: true },
-        { name: 'Levels', value: `${a.mission.minEnemyLevel} - ${a.mission.maxEnemyLevel}`, inline: true },
-      ];
+      const summary = `${a.mission.faction} ${a.mission.type} on ${a.mission.node}`;
+      this.description = a.getDescription() && a.getDescription() !== '' ? a.getDescription() : summary;
+      this.fields = [];
+      if (this.description !== summary) {
+        this.fields.push({ name: '_ _', value: `${a.mission.faction} ${a.mission.type} on ${a.mission.node}` });
+      }
+      this.fields.push({ name: '_ _', value: `**Remaining:** ${a.getETAString()} ${extraSpace}|${extraSpace}**Levels:** ${a.mission.minEnemyLevel} - ${a.mission.maxEnemyLevel}`, inline: false });
+
+      if (this.title.indexOf('cr') === -1) {
+        this.fields.push({ name: '_ _', value: `**Credits:** ${a.getReward().credits}cr`, inline: true });
+      }
     }
     this.thumbnail = {
       url: 'http://i.imgur.com/KQ7f9l7.png',
--- src/embeds/InvasionEmbed.js
+++ src/embeds/InvasionEmbed.js
@@ -14,19 +14,34 @@ class InvasionEmbed extends BaseEmbed {
     super(bot);
 
     this.color = invasions.length > 2 ? 0x00ff00 : 0xff0000;
-    this.fields = invasions.map((i) => {
+    if (invasions.length > 1) {
+      this.fields = invasions.map((i) => {
+        let rewards = i.defenderReward.toString();
+        if (!i.vsInfestation) {
+          rewards = `${i.attackerReward} vs ${rewards}`;
+        }
+
+        return {
+          name: `${rewards} - ${Math.round(i.completion * 100) / 100}%`,
+          value: `${i.desc} on ${i.node} - ETA ${i.getETAString()}`,
+        };
+      });
+      this.title = 'Worldstate - Invasions';
+      this.description = 'Currently in-progress invasions:';
+    } else {
+      const i = invasions[0];
       let rewards = i.defenderReward.toString();
       if (!i.vsInfestation) {
         rewards = `${i.attackerReward} vs ${rewards}`;
       }
+      this.title = `${rewards} - ${Math.round(i.completion * 100) / 100}%`;
+      this.description = i.desc;
+      this.fields = [
+        { name: 'Location', value: i.node, inline: true },
+        { name: 'Time Remaining', value: i.getETAString(), inline: true },
+      ];
+    }
 
-      return {
-        name: `${rewards} - ${Math.round(i.completion * 100) / 100}%`,
-        value: `${i.desc} on ${i.node} - ETA ${i.getETAString()}`,
-      };
-    });
-    this.title = 'Worldstate - Invasions';
-    this.description = 'Currently in-progress invasions:';
     this.thumbnail = {
       url: 'https://raw.githubusercontent.com/aliasfalse/genesis/master/src/resources/invasion.png',
     };
