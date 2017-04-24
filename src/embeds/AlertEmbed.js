1fb97b98fc957792922c3a7983b0de33bc58d750
Tobiah <tobiah@protonmail.com>
2017-04-24 13:18:10 -0500

    add a niicer alert color

--- src/embeds/AlertEmbed.js
+++ src/embeds/AlertEmbed.js
@@ -16,8 +16,8 @@ class AlertEmbed extends BaseEmbed {
     this.thumbnail = {
       url: 'http://i.imgur.com/KQ7f9l7.png',
     };
+    this.color = 0xF1C40F;
     if (alerts.length > 1) {
-      this.color = alerts.length > 2 ? 0x00ff00 : 0xff0000;
       this.fields = alerts.map(a => ({
         name: `${a.getReward().toString().replace(/^1\s/, '')} - ${a.getETAString()} left`,
         value: `${a.mission.faction} ${a.mission.type} on ${a.mission.node}\n` +
@@ -29,7 +29,9 @@ class AlertEmbed extends BaseEmbed {
       const a = alerts[0];
       const item = a.getReward().toString().replace(/\s*\+\s*\d*cr/ig, '').replace(/^1\s/, '');
       this.title = item === '' ? a.getReward().replace(/^1\s/, '') : item;
-      this.color = a.getReward().getTypesFull()[0].color;
+      if(a.getReward().getTypesFull()[0].color) {
+        this.color = a.getReward().getTypesFull()[0].color; 
+      }
       this.thumbnail.url = a.getReward().getTypesFull()[0].thumbnail;
       const summary = `${a.mission.faction} ${a.mission.type} on ${a.mission.node}`;
       this.description = a.getDescription() && a.getDescription() !== '' ? a.getDescription() : summary;
