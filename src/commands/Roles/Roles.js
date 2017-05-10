9ef1ebb595e0f6938aad26c7753259dde1209ebf
Tobiah <tobiah@protonmail.com>
2017-05-10 08:43:00 -0500

    fix formatting

--- src/commands/Roles/Roles.js
+++ src/commands/Roles/Roles.js
@@ -27,7 +27,7 @@ class Roles extends Command {
            fields: [
              {
                name: '_ _',
-               value: roles.length ? roles.map(role => `${rpad(role.name, 25, ' ')}${role.members.size}`).join('; ') : 'No joinable Roles',
+               value: roles.length ? `\`\`\`\n${roles.map(role => `${rpad(role.name, 25, ' ')}${role.members.size}`).join('\n')}\`\`\`` : 'No joinable Roles',
              },
            ],
          }, true, true);
