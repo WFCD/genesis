15a6ee01890b88d6ab1376685e95eee543e82290
Matej Voboril <matej@voboril.org>
2017-05-31 17:42:39 -0500

    Warframe Stats command
    add some more frame data, need the rest
    
    TODO: add the rest, add harrow, hook up to existing frame profile command
    add some more frames

---
+++ src/commands/Ondemand/FrameStats.js
@@ -0,0 +1,47 @@
+'use strict';
+
+const Command = require('../../Command.js');
+const FrameEmbed = require('../../embeds/FrameEmbed.js');
+const frames = require('../../resources/frames.json');
+
+/**
+ * Displays the stats for a warframe
+ */
+class FrameStats extends Command {
+  /**
+   * Constructs a callable command
+   * @param {Genesis} bot The bot object
+   */
+  constructor(bot) {
+    super(bot, 'warframe.misc.stats', 'stats', 'Get stats for a Warframe');
+    this.regex = new RegExp('^stats(.+)?', 'i');
+    this.usages = [
+      {
+        description: 'Get stats for a Warframe',
+        parameters: ['warframe name'],
+      },
+    ];
+  }
+
+  /**
+   * Run the command
+   * @param {Message} message Message with a command to handle, reply to,
+   *                          or perform an action based on parameters.
+   */
+  run(message) {
+    let frame = message.strippedContent.match(this.regex)[1];
+    if (frame) {
+      frame = frame.trim().toLowerCase();
+      const results = frames.filter(entry => new RegExp(entry.regex, 'ig').test(frame));
+      if (results.length > 0) {
+        this.messageManager.embed(message, new FrameEmbed(this.bot, results[0]), true, false);
+      } else {
+        this.messageManager.embed(message, new FrameEmbed(this.bot, undefined), true, false);
+      }
+    } else {
+      this.messageManager.embed(message, new FrameEmbed(this.bot, undefined), true, false);
+    }
+  }
+}
+
+module.exports = FrameStats;
---
+++ src/embeds/FrameEmbed.js
@@ -0,0 +1,81 @@
+'use strict';
+
+const BaseEmbed = require('./BaseEmbed.js');
+const frames = require('../resources/frames.json');
+
+/**
+ * Generates enemy embeds
+ */
+class FrameEmbed extends BaseEmbed {
+  /**
+   * @param {Genesis} bot - An instance of Genesis
+   * @param {Enhancement} frame - The enhancement to send info on
+   */
+  constructor(bot, frame) {
+    super();
+
+    this.thumbnail = {
+      url: 'https://i.imgur.com/tIBB0ea.png',
+    };
+    if (frame && typeof frame !== 'undefined') {
+      this.title = frame.name;
+      this.url = frame.info;
+      this.thumbnail.url = frame.thumbnail;
+      this.description = frame.description;
+      this.footer = { text: `Drops from: ${frame.location}` };
+      this.color = frame.color;
+      this.fields = [
+        {
+          name: '_ _',
+          value: 'The stats in parentheses `()` denotes the prime variant\'s stats, if one exists.',
+          inline: false,
+        },
+        {
+          name: 'Minimum Mastery',
+          value: `${frame.mr}${frame.prime_mr ? ` (${frame.prime_mr})` : ''}`,
+          inline: true,
+        },
+        {
+          name: 'Health',
+          value: `${frame.health}${frame.prime_health ? ` (${frame.prime_health})` : ''}`,
+          inline: true,
+        },
+        {
+          name: 'Shields',
+          value: `${frame.shield}${frame.prime_shield ? ` (${frame.prime_shield})` : ''}`,
+          inline: true,
+        },
+        {
+          name: 'Armor',
+          value: `${frame.armor}${frame.prime_armor ? ` (${frame.prime_armor})` : ''}`,
+          inline: true,
+        },
+        {
+          name: 'Power',
+          value: `${frame.power}${frame.prime_power ? ` (${frame.prime_power})` : ''}`,
+          inline: true,
+        },
+        {
+          name: 'Conclave',
+          value: `${frame.conclave}${frame.prime_conclave ? ` (${frame.prime_conclave})` : ''}`,
+          inline: true,
+        },
+        {
+          name: 'Aura',
+          value: `${frame.aura || 'No Aura'}${frame.prime_aura ? ` (${frame.prime_aura})` : ''}`,
+          inline: true,
+        },
+        {
+          name: 'Polarities',
+          value: `${frame.polarities ? frame.polarities.join(', ') : 'No polarities'}${frame.prime_polarities ? ` (${frame.prime_polarities.join(', ')})` : ''}`,
+          inline: false,
+        },
+      ];
+    } else {
+      this.title = 'Available Warframes';
+      this.fields = [{ name: '_ _', value: frames.map(stat => stat.name).join('\n') }];
+    }
+  }
+}
+
+module.exports = FrameEmbed;
