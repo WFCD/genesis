e4fa366b23ca6db886d186bcfaf06cebdb6a2aca
Tobiah <tobiah@protonmail.com>
2017-08-10 10:11:55 -0500

    test change for setting welcome messages

--- src/settings/Database.js
+++ src/settings/Database.js
@@ -955,7 +955,7 @@ class Database {
    * @returns {Promise}
    */
   setWelcome(message, isDm, text) {
-    const query = SQL`INSERT INTO welcome_messages VALUES (${message.guild.id}, ${isDm}, ${message.channel.id}, ${text})
+    const query = SQL`INSERT INTO welcome_messages VALUES (${message.guild.id}, ${isDm}, ${message.channel.id}, ${text}, 1)
       ON DUPLICATE KEY UPDATE message = ${text};`;
     return this.db.query(query);
   }
