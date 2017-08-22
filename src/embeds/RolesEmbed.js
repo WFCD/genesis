9fa9f11eab31680d16e9e6441d9e3455912a54d6
Tobiah <tobiah@protonmail.com>
2017-08-22 13:41:57 -0500

    role groups modification

--- src/embeds/RolesEmbed.js
+++ src/embeds/RolesEmbed.js
@@ -9,24 +9,31 @@ const rpad = require('right-pad');
 class RolesEmbed extends BaseEmbed {
   /**
    * @param {Genesis} bot - An instance of Genesis
-   * @param {Array.<Role>} roles - The array of roles allowed to be used
+   * @param {Array.<Array.<Role>>} roleGroups - An array of an array of roles allowed to be used
    * @param {string} prefix - prefix for the command
    * @param {number} longest - length of longest name so that all are aligned
    */
-  constructor(bot, roles, prefix, longest) {
+  constructor(bot, roleGroups, prefix, longest) {
     super();
     this.title = 'Joinable Roles';
     this.type = 'rich';
     this.color = 0x779ECB;
-    this.fields = [
-      {
+    this.fields = [];
+    roleGroups.forEach((roleGroup) => {
+      this.fields.push({
         name: '_ _',
-        value: roles.length ? `${roles.map(role => `\`${rpad(role.name, Number(longest + 2), ' ')}${rpad(String(role.members.size), 4, ' ')} members\``).join(' \n')}` : 'No joinable Roles',
-      },
-      {
+        value: `${roleGroup.map(role => `\`${rpad(role.name, Number(longest + 2), ' ')}${rpad(String(role.members.size), 4, ' ')} members\``).join(' \n')}`,
+      });
+    });
+    
+    if (roleGroups.length === 0) {
+      this.fields.push({ name: '_ _', value: 'No joinable Roles' });
+    }
+      
+    this.fields.push({
         name: '_ _',
-        value: roles.length ? `**Use the \`${prefix}join\` command to join a role**` : `Use the \`${prefix}add role <role>\` command to make a role joinable.`,
-      },
+        value: roleGroups.length ? `**Use the \`${prefix}join\` command to join a role**` : `Use the \`${prefix}add role <role>\` command to make a role joinable.`,
+      }),
     ];
   }
 }
