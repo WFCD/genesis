ee5c7e5475163438fe523d9867f517a611da741a
Tobiah <tobiah@protonmail.com>
2017-08-23 15:06:05 -0500

    fix verification stuff for server info

--- src/commands/Utilities/ServerInfo.js
+++ src/commands/Utilities/ServerInfo.js
@@ -2,8 +2,8 @@
 
 const Command = require('../../Command.js');
 
-const verificationLevels = ['None', 'Low\nMust have a verified email on their Discord Account', 'Medium\nMust also be registered on Discord for longer than 5 minutes', '(╯°□°）╯︵ ┻━┻\nMust also be a member of this server for longer than 10 minutes.'];
-const verificationColors = [0x747f8d, 0x43b581, 0xfaa61a, 0xf04747];
+const verificationLevels = ['None', 'Low\nMust have a verified email on their Discord Account', 'Medium\nMust also be registered on Discord for longer than 5 minutes', '(╯°□°）╯︵ ┻━┻\nMust also be a member of this server for longer than 10 minutes.', 'Insane: ┻━┻ ﾐヽ(ಠ益ಠ)ノ彡┻━┻\nMust have a verified phone on their Discord Account.'];
+const verificationColors = [0x747f8d, 0x43b581, 0xfaa61a, 0xf57731, 0xf04747];
 
 /**
  * Get a list of all servers
