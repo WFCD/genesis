2bb1201f2b6fc589999455595ccd97c99f79bb77
Tobiah <tobiah@protonmail.com>
2017-08-28 07:44:22 -0500

    buildchart

---
+++ src/commands/Ondemand/BuildChart.js
@@ -0,0 +1,34 @@
+'use strict';
+
+const Command = require('../../Command.js');
+
+const buildChart = 'http://i.imgur.com/EOzr440.png';
+
+/**
+ * Displays the weapon crafting chart
+ */
+class BuildChart extends Command {
+  /**
+   * Constructs a callable command
+   * @param {Genesis} bot  The bot object
+   */
+  constructor(bot) {
+    super(bot, 'warframe.misc.buildchart', 'buildchart', 'Display Weapon Build Flowchart chart');
+  }
+
+  /**
+   * Run the command
+   * @param {Message} message Message with a command to handle, reply to,
+   *                          or perform an action based on parameters.
+   */
+  run(message) {
+    this.messageManager.embed(message, {
+      title: 'Legend',
+      image: {
+        url: buildChart,
+      },
+    }, true, false);
+  }
+}
+
+module.exports = BuildChart;
