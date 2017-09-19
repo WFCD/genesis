3d78ff78040b787423d80345602fb2502129f2e8
Tobiah <tobiah@protonmail.com>
2017-09-19 14:59:34 -0500

    adding embeds

---
+++ src/embeds/ConstructionEmbed.js
@@ -0,0 +1,29 @@
+'use strict';
+
+const BaseEmbed = require('./BaseEmbed.js');
+
+/**
+ * Generates daily deal embeds
+ */
+class ConstructionEmbed extends BaseEmbed {
+  /**
+   * @param {Genesis} bot - An instance of Genesis
+   * @param {Construction} constructionProgress - The current construction information
+   * @param {string} platform - The platform the event is for
+   */
+  constructor(bot, constructionProgress, platform) {
+    super();
+
+    this.color = 0xff6961;
+    this.fields = [{
+        name: `[${platform}] Construction Status:`,
+        value: '```' +
+        `Razorback: ${construction.razorbackProgress}` +
+        `Fomorian:  ${construction.fomorianProgress}` +
+        `Unknown:   ${construction.unknwonProgress}` +
+        '```'
+    }];
+  }
+}
+
+module.exports = ConstructionEmbed;
