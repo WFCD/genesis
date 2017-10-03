c9e9d1ce776c911012cd984d679802fbf4890e58
Matej Voboril <matej@voboril.org>
2017-10-02 21:44:36 -0500

    more fields for the setfield command

--- src/commands/Builds/SetField.js
+++ src/commands/Builds/SetField.js
@@ -29,33 +29,42 @@ class AddBuild extends Command {
       // let them know there's not enough params
       return this.messageManager.statuses.FAILURE;
     }
-    if (type === 'all') {
-      const buildId = params[0];
-      const build = await this.bot.settings.getBuild(buildId);
-      const title = params[1] || 'My Build';
-      const body = params[2] || 'My Build Body';
-      const image = params[3] || 'https://i.imgur.com/31xCos6.png';
-      if (build && build.owner_id === message.owner.id) {
-        const status = await this.settings.setBuildFileds(buildId, { title, body, image });
-        if (status) {
-          this.messageManager.embed(message, new BuildEmbed(this.bot,
-            await this.settings.getBuild(buildId)), true, true);
-          return this.messageManager.statuses.SUCCESS;
-        }
-        this.messageManager.embed(message, { title: `You couldn't edit build ${buildId}.`, color: 0x83181b }, true, true);
-        return this.messageManager.statuses.FAILURE;
+    const buildId = params[0];
+    const build = await this.bot.settings.getBuild(buildId);
+    let title;
+    let body;
+    let image;
+    if (build && build.owner_id === message.owner.id) {
+      if (type === 'all') {
+        title = params[1];
+        body = params[2];
+        image = params[3];
+      } else if (type === 'title') {
+        title = params[1];
+      } else if (type === 'body') {
+        body = params[1];
+      } else if (type === 'image') {
+        image = params[1];
+      } else {
+        return this.failure(message, buildId);
       }
-      this.messageManager.embed(message, { title: `You couldn't edit build ${buildId}.`, color: 0x83181b }, true, true);
-      return this.messageManager.statuses.FAILURE;
+    } else {
+      return this.failure(message, buildId);
     }
+
     // save params based on order
-    const title = params[0] || 'My Build';
-    const body = params[1] || 'My Build Body';
-    const image = params[2] || 'https://i.imgur.com/31xCos6.png';
-    const build = await this.bot.settings.addNewBuild(title, body, image, message.author);
-    const embed = new BuildEmbed(this.bot, build);
-    this.messageManager.embed(message, embed, true, true);
-    return this.messageManager.statuses.SUCCESS;
+    const status = await this.settings.setBuildFileds(buildId, { title, body, image });
+    if (status) {
+      this.messageManager.embed(message, new BuildEmbed(this.bot,
+        await this.settings.getBuild(buildId)), true, true);
+      return this.messageManager.statuses.SUCCESS;
+    }
+    return this.failure(message, buildId);
+  }
+
+  async failure(message, buildId) {
+    this.messageManager.embed(message, { title: `You couldn't edit build ${buildId}.`, color: 0x83181b }, true, true);
+    return this.messageManager.statuses.FAILURE;
   }
 }
 
