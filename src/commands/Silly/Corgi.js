a23019e6270f5e2c5a3713d38e2d06ebf83cd4ef
Tobiah <tobiah@protonmail.com>
2017-10-09 09:49:29 -0500

    corgi command

---
+++ src/commands/Silly/Corgi.js
@@ -0,0 +1,35 @@
+'use strict';
+
+const Command = require('../../Command.js');
+const corggit = require('../../resources/Corggit.js');
+
+/**
+ * Corgis - Bsed on https://github.com/ryands/hubot-corgi
+ */
+class Corgi extends Command {
+  /**
+   * Constructs a callable command
+   * @param {Genesis} bot  The bot object
+   */
+  constructor(bot) {
+    super(bot, 'silly.corgi', 'corgi', 'Genesis gets you a corgi');
+  }
+
+  /**
+   * Run the command
+   * @param {Message} message Message with a command to handle, reply to,
+   *                          or perform an action based on parameters.
+   * @returns {string} success status
+   */
+  async run(message) {
+	const corgi = await corggit();
+	if (corgi) {
+		await this.messageManager.sendMessage(message, corgi.url, true, true);
+		return this.messageManager.statuses.SUCCESS;
+	} else {
+		return this.messageManager.statuses.FAILURE;
+	}
+  }
+}
+
+module.exports = Corgi;
