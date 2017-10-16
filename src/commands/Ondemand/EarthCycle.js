725ed30c7b05f8cf91297f9f850bc49726b2d7c3
Tobiah <tobiah@protonmail.com>
2017-10-16 12:29:53 -0500

    fix tabs

--- src/commands/Ondemand/EarthCycle.js
+++ src/commands/Ondemand/EarthCycle.js
@@ -13,7 +13,7 @@ class EarthCycle extends Command {
    */
   constructor(bot) {
     super(bot, 'warframe.misc.cycle', 'cycle', 'Current and remaining time in cycle of Earth rotation.');
-	this.regex = new RegExp(`^${this.call}\\s?(earth)?`, 'i');
+    this.regex = new RegExp(`^${this.call}\\s?(earth)?`, 'i');
   }
 
   /**
@@ -23,14 +23,14 @@ class EarthCycle extends Command {
    * @returns {string} success status
    */
   async run(message) {
-  	let cycleData;
+    let cycleData;
     const matches = message.content.match(this.regex);
     const ws = await this.bot.caches.pc.getDataJson();
-  	if (matches ? matches[1] : false) {
-  		cycleData = ws.earthCycle;
-  	} else {
+    if (matches ? matches[1] : false) {
+      cycleData = ws.earthCycle;
+    } else {
       cycleData = ws.cetusCycle;
-  	}
+    }
     const embed = new EarthCycleEmbed(this.bot, cycleData);
     await this.messageManager.embed(message, embed, true, true);
     return this.messageManager.statuses.SUCCESS;
