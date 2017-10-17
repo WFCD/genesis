ffe08fbe4050fa9855afc6cce3f9c337b9febadf
Tobiah <tobiah@protonmail.com>
2017-10-17 10:58:35 -0500

    reorg syndicate job

--- src/embeds/SyndicateEmbed.js
+++ src/embeds/SyndicateEmbed.js
@@ -25,8 +25,8 @@ class SyndicateEmbed extends BaseEmbed {
         .map(m => (
           {
             name: m.syndicate,
-            value: m.jobs.length ? `${m.jobs.map(job => `${job.type} (${job.enemyLevels.join(' - ')}) ` +
-              `<:standing:369875864004984832> ${job.standingStages.reduce((a, b) => a + b, 0)}`).join('\n')}\n\nExpires in ${m.eta}`
+            value: m.jobs.length ? `${m.jobs.map(job => `<:standing:369875864004984832> ${job.standingStages.reduce((a, b) => a + b, 0)}`).join('\n')}  - ` +
+              `${job.type} (${job.enemyLevels.join(' - ')})\n\nExpires in ${m.eta}`
               : `${m.nodes.join('\n')}\n\nExpires in ${m.eta}`,
             inline: true,
           }));
