02f875bdd5135e29b6fdd86cc67a726d283bb62c
Tobiah <tobiah@protonmail.com>
2017-10-21 09:22:50 -0700

    tweak

--- src/embeds/SyndicateEmbed.js
+++ src/embeds/SyndicateEmbed.js
@@ -49,7 +49,7 @@ class SyndicateEmbed extends BaseEmbed {
           return {
             name: syndMissions.length < 2 ? '_ _' : m.syndicate,
             value,
-            inline: m.jobs.length,
+            inline: m.jobs.length > 0,
           };
         });
       }
