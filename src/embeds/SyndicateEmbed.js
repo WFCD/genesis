8c1253ac46206d3ae2a4e87a4146af2120dec046
Tobiah <tobiah@protonmail.com>
2017-10-21 09:10:18 -0700

    fix for undefined property on syndicate embed (name)

--- src/embeds/SyndicateEmbed.js
+++ src/embeds/SyndicateEmbed.js
@@ -17,24 +17,33 @@ class SyndicateEmbed extends BaseEmbed {
   constructor(bot, missions, syndicate, platform) {
     super();
 
-    const foundSyndicate = values.find(v => syndicate &&
+    const foundSyndicate = missions.length && values.find(v => syndicate &&
       v.toLowerCase() === syndicate.toLowerCase());
     if (foundSyndicate) {
-      this.title = `[${platform.toUpperCase()}] Syndicates`;
-      this.color = 0x00ff00;
       const syndMissions = missions.filter(m => m.syndicate === foundSyndicate || foundSyndicate === 'all');
-      if (syndMissions.length < 2) {
-        this.title = `[${platform.toUpperCase()}] ${syndMissions[0].syndicate}`;
-        this.footer.text = `${this.footer.text} | \n\nExpires in ${syndMissions[0].eta}`;
+      if (syndMissions.length) {
+        this.title = `[${platform.toUpperCase()}] Syndicates`;
+        this.color = 0x00ff00;
+        const syndMissions = missions.filter(m => m.syndicate === foundSyndicate || foundSyndicate === 'all');
+        if (syndMissions.length < 2) {
+          this.title = `[${platform.toUpperCase()}] ${syndMissions[0].syndicate}`;
+          this.footer.text = `${this.footer.text} | \n\nExpires in ${syndMissions[0].eta}`;
+        }
+        this.fields = syndMissions.map(m => (
+          {
+            name: syndMissions.length < 2 ? '_ _' : m.syndicate,
+            value: m.jobs.length ? `${m.jobs.map(job => `<:standing:369875864004984832> ${job.standingStages.reduce((a, b) => a + b, 0)} ` +
+                `- ${job.type} (${job.enemyLevels.join(' - ')})`).join('\n')}${syndMissions.length < 2 ? '' : `\n\nExpires in ${m.eta}`}`
+              : `${m.nodes.join('\n')}`,
+            inline: true,
+          }));
+      } else {
+        this.color = 0xff0000;
+        this.fields = [{
+          name: 'No such Syndicate',
+          value: `Valid values: ${values.join(', ')}`,
+        }];
       }
-      this.fields = syndMissions.map(m => (
-        {
-          name: syndMissions.length < 2 ? '_ _' : m.syndicate,
-          value: m.jobs.length ? `${m.jobs.map(job => `<:standing:369875864004984832> ${job.standingStages.reduce((a, b) => a + b, 0)} ` +
-              `- ${job.type} (${job.enemyLevels.join(' - ')})`).join('\n')}${syndMissions.length < 2 ? '' : `\n\nExpires in ${m.eta}`}`
-            : `${m.nodes.join('\n')}`,
-          inline: true,
-        }));
     } else {
       this.color = 0xff0000;
       this.fields = [{
