32619f699d2d4bd97eb751bea22699d0b54d105d
Tobiah <tobiah@protonmail.com>
2017-10-27 09:41:49 -0500

    refactor help

--- src/commands/Core/Help.js
+++ src/commands/Core/Help.js
@@ -2,6 +2,11 @@
 
 const Command = require('../../Command.js');
 
+const invalidResultsEmbed = {
+  color: 0x00CCFF,
+  title: 'No results, please refine query.',
+};
+
 
 /**
  * Describes the Help command
@@ -15,6 +20,19 @@ class Help extends Command {
     super(bot, 'core.help', 'help', 'Display this message');
 
     this.helpEmbed = null;
+
+    this.regex = new RegExp(`^${this.call}\\s?(.*)?`, 'i');
+
+    this.usages = [
+      {
+        description: 'Receive the full welcome data by visiting the provided link.',
+        parameters: [],
+      },
+      {
+        description: 'Query for info about a specific command',
+        parameters: ['command search'],
+      },
+    ];
 
     /**
      * Help reply messsage for alerting a user to check their direct messages.
@@ -30,105 +48,68 @@ class Help extends Command {
    * @returns{boolean} success status
    */
   async run(message) {
-    if (message.channel.type !== 'dm') {
-      this.messageManager.reply(message, this.helpReplyMsg, true, false);
+    let query = message.strippedContent.match(this.regex)[1];
+
+    if (query) {
+      query = query.toLowerCase();
+      if (message.channel.type !== 'dm') {
+        await this.messageManager.reply(message, this.helpReplyMsg, true, true);
+      }
+      const config = {
+        prefix: await this.bot.settings.getChannelSetting(message.channel, 'prefix'),
+        isOwner: message.author.id === this.bot.owner,
+        hasAuth: message.channel.type === 'dm' || message.channel
+          .permissionsFor(message.author)
+          .has('MANAGE_ROLES_OR_PERMISSIONS'),
+      };
+
+      const searchableCommands = this.commandHandler.commands.filter((command) => {
+        if ((command.ownerOnly && !config.isOwner) || (command.requiresAuth && !config.hasAuth)) {
+          return false;
+        }
+        return true;
+      });
+
+        // filter commands
+      let matchingCommands = searchableCommands.filter((command) => {
+        if (query.length < 3) {
+          return false;
+        }
+        return command.call.toLowerCase().includes(query)
+          || command.id.toLowerCase().includes(query)
+          || command.usages.filter(usage => usage.description.toLowerCase().includes(query)
+          || usage.parameters.join(' ').toLowerCase().includes(query)).length > 0;
+      });
+
+      if (matchingCommands.length < 1) {
+        await this.messageManager.sendDirectEmbedToAuthor(message, invalidResultsEmbed, false);
+        return this.messageManager.statuses.FAILURE;
+      }
+      matchingCommands = matchingCommands.slice(0, 10).map(command => command.usages.map(u => ({
+        name: `${config.prefix}${command.call} ${u.parameters.map(p => `<${p}>`).join(u.separator ? u.separator : ' ')}`,
+        value: u.description,
+        inline: false,
+      })));
+      await this.sendEmbedForCommands(message, matchingCommands, 'Help!', 0x4068BD);
+      return this.messageManager.statuses.SUCCESS;
     }
-    const prefix = await this.bot.settings.getChannelSetting(message.channel, 'prefix');
-    this.sendCoreEmbed(message, prefix);
-    if (message.channel.type === 'dm' ||
-       message.channel
-         .permissionsFor(message.author)
-         .has('MANAGE_ROLES_OR_PERMISSIONS')) {
-      this.sendSettingsEmbed(message, prefix);
-    }
-    this.sendWorldStateEmbed(message, prefix);
-    this.sendWarframeEmbed(message, prefix);
-    this.sendHelpEmbed(message, prefix);
-    if (message.author.id === this.bot.owner) {
-      this.sendOwnerOnlyEmbed(message, prefix);
-    }
+    await this.messageManager.reply(message, `Visit ${process.env.HELP_URL || 'http://genesis.warframestat.us'} for all the commands and usages!`, true, true);
     return this.messageManager.statuses.SUCCESS;
   }
 
-  sendHelpEmbed(message, prefix) {
-    const commands = this.commandHandler.commands.filter(c =>
-      !c.ownerOnly &&
-        !/core/ig.test(c.id) &&
-        !(/warframe/ig.test(c.id)) &&
-        !/settings/ig.test(c.id))
-      .map(c => c.usages.map(u => ({
-        name: `${prefix}${c.call} ${u.parameters.map(p => `<${p}>`).join(u.separator ? u.separator : ' ')}`,
-        value: u.description,
-        inline: false,
-      }
-      )));
-    this.sendEmbedForCommands(message, commands, 'Help!', 0x00ff00);
-  }
-
-  sendOwnerOnlyEmbed(message, prefix) {
-    const ownerCommands = this.commandHandler.commands.filter(c => c.ownerOnly)
-      .map(c => c.usages.map(u => ({
-        name: `${prefix}${c.call} ${u.parameters.map(p => `<${p}>`).join(u.separator ? u.separator : ' ')}`,
-        value: u.description,
-        inline: false,
-      })));
-    this.sendEmbedForCommands(message, ownerCommands, 'Owner Only', 0xff0000);
-  }
-
-  sendCoreEmbed(message, prefix) {
-    const commands = this.commandHandler.commands.filter(c => !c.ownerOnly && /core/ig.test(c.id))
-      .map(c => c.usages.map(u => ({
-        name: `${prefix}${c.call} ${u.parameters.map(p => `<${p}>`).join(u.separator ? u.separator : ' ')}`,
-        value: u.description,
-        inline: false,
-      })));
-    this.sendEmbedForCommands(message, commands, 'Core Commands', 0x000000);
-  }
-
-  sendWorldStateEmbed(message, prefix) {
-    const commands = this.commandHandler.commands.filter(c => !c.ownerOnly && /warframe.worldstate/ig.test(c.id))
-      .map(c => c.usages.map(u => ({
-        name: `${prefix}${c.call} ${u.parameters.map(p => `<${p}>`).join(u.separator ? u.separator : ' ')}`,
-        value: u.description,
-        inline: false,
-      })));
-    this.sendEmbedForCommands(message, commands, 'Warframe Commands - Worldstate', 0x4068BD);
-  }
-
-  sendWarframeEmbed(message, prefix) {
-    const commands = this.commandHandler.commands.filter(c => !c.ownerOnly && /warframe.(?!worldstate)/ig.test(c.id))
-      .map(c => c.usages.map(u => ({
-        name: `${prefix}${c.call} ${u.parameters.map(p => `<${p}>`).join(u.separator ? u.separator : ' ')}`,
-        value: u.description,
-        inline: false,
-      })));
-    this.sendEmbedForCommands(message, commands, 'Warframe Commands - Utility', 0x4068BD);
-  }
-
-  sendSettingsEmbed(message, prefix) {
-    const ownerCommands = this.commandHandler.commands.filter(c => !c.ownerOnly && /settings/ig.test(c.id))
-      .map(c => c.usages.map(u => ({
-        name: `${prefix}${c.call} ${u.parameters.map(p => `<${p}>`).join(u.separator ? u.separator : ' ')}`,
-        value: u.description,
-        inline: false,
-      })));
-    this.sendEmbedForCommands(message, ownerCommands, 'Settings Commands', 0xe5c100);
-  }
-
-  sendEmbedForCommands(message, commands, title, color) {
+  async sendEmbedForCommands(message, commands, title, color) {
     const embed = {
       title,
       fields: [].concat(...commands),
       color,
+      type: 'rich',
+      footer: {
+        text: commands.length === 10 ? 'Search results limited to 10. If you believe more are available, please refine your search.' : undefined,
+        icon_url: 'https://raw.githubusercontent.com/WFCD/genesis/master/src/resources/genesis-vector-cyan.png',
+      },
     };
-    if (title === 'Core Commands') {
-      embed.type = 'rich';
-      embed.thumbnail = {
-        url: 'https://github.com/aliasfalse/genesis/raw/master/src/resources/cephalontransparent.png',
-      };
-    }
     if (commands.length > 0) {
-      this.messageManager.sendDirectEmbedToAuthor(message, embed, false);
+      await this.messageManager.sendDirectEmbedToAuthor(message, embed, false);
     }
   }
 }
