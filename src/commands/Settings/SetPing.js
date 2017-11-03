6806e76a476882105038666bbcb52d9680da9f7a
Tobiah <tobiah@protonmail.com>
2017-11-03 10:06:53 -0500

    remove debugging statements

--- src/commands/Settings/SetPing.js
+++ src/commands/Settings/SetPing.js
@@ -18,10 +18,8 @@ class SetPing extends Command {
   }
 
   async run(message) {
-    this.logger.debug('Executing set ping');
     const regex = new RegExp(`(${callables.join('|')})(.+)?`, 'i');
     const match = message.content.match(regex);
-    this.logger.debug(`Matches: ${JSON.stringify(match)}`);
     if (message.channel.type === 'dm') {
       this.messagemanager.reply(message, 'Operator, you can\'t do that privately, it\'s the same as directly messaging you anyway!');
       return this.messageManager.statuses.FAILURE;
