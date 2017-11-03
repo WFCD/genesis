c9297f5891be156f413f51f6516086f2a1440c71
Tobiah <tobiah@protonmail.com>
2017-11-03 10:19:04 -0500

    Fix no param call not showing instructions (#143)
    * Update SetPing.js
    
    * fix debug template string
    
    * allow any amount of spaces so it responds to no params
    
    * remove debugging statements
    
    * fix quotes
    
    * spacing in array

--- src/commands/Settings/SetPing.js
+++ src/commands/Settings/SetPing.js
@@ -4,19 +4,21 @@ const Command = require('../../Command.js');
 const trackFunctions = require('../../TrackFunctions.js');
 const { eventTypes, rewardTypes } = require('../../resources/trackables.json');
 
+const callables = eventTypes.concat(rewardTypes).concat(['all', 'events', 'items', 'fissures', 'syndicates', 'conclave', 'resources', 'deals', 'clantech']);
+
 class SetPing extends Command {
   constructor(bot) {
     super(bot, 'settings.setping', 'set ping');
     this.usages = [
       { description: 'Set ping for an event or item', parameters: ['event or reward', '@role or user mention'] },
     ];
-    this.regex = new RegExp(`^${this.call}\\s+?((${eventTypes.join('|')}|${rewardTypes.join('|')}|all|events|items|fissures|syndicates|conclave|resources|deals|clantech)(.+)?)?$`, 'i');
+    this.regex = new RegExp(`^${this.call}\\s*((${callables.join('|')})(.+)?)?`, 'i');
     this.requiresAuth = true;
     this.allowDM = false;
   }
 
   async run(message) {
-    const regex = new RegExp(`(${eventTypes.join('|')}|${rewardTypes.join('|')}|all|events|items|fissures|syndicates|conclave|resources|deals|clantech)(.+)?`, 'i');
+    const regex = new RegExp(`(${callables.join('|')})(.+)?`, 'i');
     const match = message.content.match(regex);
     if (message.channel.type === 'dm') {
       this.messagemanager.reply(message, 'Operator, you can\'t do that privately, it\'s the same as directly messaging you anyway!');
@@ -51,7 +53,7 @@ class SetPing extends Command {
       return this.messageManager.statuses.SUCCESS;
     }
     const prefix = await this.bot.settings.getChannelSetting(message.channel, 'prefix');
-    this.messageManager.embed(message, trackFunctions
+    await this.messageManager.embed(message, trackFunctions
       .getTrackInstructionEmbed(message, prefix, this.call), true, true);
     return this.messageManager.statuses.FAILURE;
   }
