4714b0c28f1863ef44248abf4b9543d8f55a0efc
Tobiah <tobiah@protonmail.com>
2017-11-09 11:54:04 -0600

    remove code block attempt (#145)
    * remove code block attempt
    
    * backticks with "proper" escaping
    
    * another formatting test
    
    * move description to field
    
    * separate some of the formatting into different parts
    
    fix expansions
    
    * not enough escapes
    
    * remove blocks again
    
    * try moving backtick to var
    
    * move backticks inside link mask
    
    * use buildid, remove avatar image
    
    * Update fail.sh

--- fail.sh
+++ fail.sh
@@ -1,3 +1,6 @@
+#!/usr/bin/env bash
 echo 'Sending Discord Webhook';
+export BACKTICK='`';
 export TIMESTAMP=$(date --utc +%FT%TZ);
-curl -v -H User-Agent:bot -H Content-Type:application/json -d '{"avatar_url": "https://i.imgur.com/kOfUGNS.png", "username": "Travis CI", "embeds": [{"author": {"name": "'"$AUTHOR_NAME"'", "icon_url": "https://github.com/'"$AUTHOR_NAME"'.png"}, "url": "https://github.com/WFCD/Genesis/commit/'"$TRAVIS_COMMIT"'", "title": "['"$TRAVIS_REPO_SLUG"':'"$TRAVIS_BRANCH"'] Build #'"$TRAVIS_BUILD_NUMBER"' Failed", "color": 16711680,"description": "'"$TRAVIS_COMMIT_MESSAGE"'", "timestamp": "'"$TIMESTAMP"'"}]}' $DISCORD_WEBHOOK_URL;
+export COMMIT_FORMATTED="[$BACKTICK${TRAVIS_COMMIT:0:7}$BACKTICK](https://github.com/$REPO_OWNER/$REPO_NAME/commit/$TRAVIS_COMMIT)";
+curl -v -H User-Agent:bot -H Content-Type:application/json -d '{"avatar_url":"https://i.imgur.com/kOfUGNS.png","username":"Travis CI","embeds":[{"author":{"name":"Build #'"$TRAVIS_BUILD_NUMBER"' Failed - '"$AUTHOR_NAME"'","url":"https://travis-ci.org/'"$REPO_OWNER"'/'"$REPO_NAME"'/builds/'"$TRAVIS_BUILD_ID"'"},"url":"https://github.com/'"$REPO_OWNER"'/'"$REPO_NAME"'/commit/'"$TRAVIS_COMMIT"'","title":"['"$TRAVIS_REPO_SLUG"':'"$TRAVIS_BRANCH"'] ","color":16711680,"fields":[{"name":"_ _", "value": "'"$COMMIT_FORMATTED"' - '"$TRAVIS_COMMIT_MESSAGE"'"}],"timestamp":"'"$TIMESTAMP"'","footer":{"text":"Node Version: '"$TRAVIS_NODE_VERSION"'"}}]}' $DISCORD_WEBHOOK_URL;
