01d2e1eaacd00cf2cfac527b4834d718793670cb
Tobiah <tobiah@protonmail.com>
2017-11-17 14:12:14 -0600

    Update EventEmbed.js

--- src/embeds/EventEmbed.js
+++ src/embeds/EventEmbed.js
@@ -19,6 +19,7 @@ class EventEmbed extends BaseEmbed {
     if (event) {
       this.title = `[${platform.toUpperCase()}] ${event.description}`;
       this.fields = [];
+      this.description = event.tooltip;
 
       if (event.victimNode) {
         this.fields.push({
@@ -26,8 +27,24 @@ class EventEmbed extends BaseEmbed {
           value: `Defend ${event.victimNode} by attacking the ${event.faction} at ${event.node}.`,
         });
       }
-      this.fields.push({ name: 'Rewards', value: event.rewards ? event.rewards.map(reward => reward.asString).join('; ') : 'No Rewards' });
-      this.fields.push({ name: 'Completion Score', value: String(event.maximumScore) });
+      if (event.rewards) {
+        this.fields.push({ name: 'Rewards', value: event.rewards ? event
+                          .rewards.map(reward => reward.asString).join('; ') : 'No Rewards' });
+      }
+      if (event.maximumScore) {
+        this.fields.push({ name: 'Completion Score', value: String(event.maximumScore) });
+      }
+      
+      if (event.affiliatedWith) {
+        const jobString = event.jobs.map((job) => {
+          const rewards = job.rewardPool.length > 0 ? job.rewardPool.map(reward => `* ${reward}`).join('\n') : 'No Clear Reward';
+          const rewardsFmtd = `\nWith \`\`\`${rewards}\`\`\` as reward${job.rewardPool.length > 1 ? 's' : ''}.`;
+          const desc = `**${job.type}}**\nEnemies lvls ${job.enemyLevels[0]} - ${job.enemyLevels[1]}\n` + 
+            `Granting ${event.standingStages.join(' ,')} standing per stage (without modification).${job.rewardPool.length > 0 ? rewards : ''}`;
+          return ;
+        }).join('\n');
+        this.fields.push({ name: `Jobs from ${event.affiliatedWith`, value: `` });
+      }
 
       if (event.health) {
         this.footer.text = `${event.health}% Remaining | ${this.footer.text}`;
