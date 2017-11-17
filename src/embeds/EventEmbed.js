8867ef495509d3acad6827ad26a33d2d17bf5285
Tobiah <tobiah@protonmail.com>
2017-11-17 14:15:33 -0600

    jobs have standing stages, not events

--- src/embeds/EventEmbed.js
+++ src/embeds/EventEmbed.js
@@ -40,7 +40,7 @@ class EventEmbed extends BaseEmbed {
           const rewards = job.rewardPool.length > 0 ? job.rewardPool.map(reward => `* ${reward}`).join('\n') : 'No Clear Reward';
           const rewardsFmtd = `\nWith \`\`\`${rewards}\`\`\` as reward${job.rewardPool.length > 1 ? 's' : ''}.`;
           const desc = `**${job.type}}**\nEnemies lvls ${job.enemyLevels[0]} - ${job.enemyLevels[1]}\n` + 
-            `Granting ${event.standingStages.join(' ,')} standing per stage (without modification).${job.rewardPool.length > 0 ? rewards : ''}`;
+            `Granting ${job.standingStages.join(' ,')} standing per stage (without modification).${job.rewardPool.length > 0 ? rewards : ''}`;
           return desc;
         }).join('\n');
         this.fields.push({ name: `Jobs from ${event.affiliatedWith}`, value: jobString });
