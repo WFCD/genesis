a3df9d91b9b237f7fb2fad119e4396f4bbcb8c6c
Matej Voboril <matej@voboril.org>
2017-11-19 18:55:24 -0600

    add echo command at patron's request

---
+++ src/commands/Silly/Echo.js
@@ -0,0 +1,33 @@
+'use strict';
+
+const Command = require('../../Command.js');
+
+/**
+ * Echo what the user says
+ */
+class Echo extends Command {
+  /**
+   * Constructs a callable command
+   * @param {Genesis} bot  The bot object
+   */
+  constructor(bot) {
+    super(bot, 'silly.echo', 'echo', 'Genesis echos your message');
+    this.regex = new RegExp(`^${this.call}\\s?(.+)?`, 'i');
+  }
+
+  /**
+   * Run the command
+   * @param {Message} message Message with a command to handle, reply to,
+   *                          or perform an action based on parameters.
+   * @returns {string} success status
+   */
+  async run(message) {
+    if (message.deleteable) {
+      await message.delete();
+    }
+    await this.messageManager.sendMessage(message, message.strippedContent.replace(this.call, '').trim(), false, true);
+    return this.messageManager.statuses.SUCCESS;
+  }
+}
+
+module.exports = Echo;
