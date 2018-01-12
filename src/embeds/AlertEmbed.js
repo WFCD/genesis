4df2e2ad72c69f790017d86a953a0db075ce5ce8
Tobiah <tobiah@protonmail.com>
2018-01-12 11:04:03 -0600

    adding some alerts and notifier optimizations and customizations

--- src/embeds/AlertEmbed.js
+++ src/embeds/AlertEmbed.js
@@ -18,33 +18,39 @@ class AlertEmbed extends BaseEmbed {
       url: 'http://i.imgur.com/KQ7f9l7.png',
     };
     this.color = 0xF1C40F;
+    // compact
     if (alerts.length > 1) {
       this.fields = alerts.map(a => ({
-        name: `${a.mission.reward.asString} - ${a.eta} left`,
+        name: `${a.mission.reward.asString} | ${a.eta} left`,
         value: `${a.mission.faction} ${a.mission.type} on ${a.mission.node}\n` +
-        `level ${a.mission.minEnemyLevel} - ${a.mission.maxEnemyLevel}`,
+        `level ${a.mission.minEnemyLevel} - ${a.mission.maxEnemyLevel}\n\u200B`,
       }));
       this.title = `[${platform.toUpperCase()}] Worldstate - Alerts`;
-    } else {
+    } else { // divided
       const a = alerts[0];
-      this.title = `[${platform.toUpperCase()}] ${a.mission.reward.itemString}`;
+      this.title = `[${platform.toUpperCase()}] ${a.mission.reward.itemString || `${a.mission.reward.credits} Credits`}`;
       this.color = a.mission.reward.color;
       this.thumbnail.url = a.mission.reward.thumbnail;
       const summary = `${a.mission.faction} ${a.mission.type} on ${a.mission.node}`;
-      this.description = a.description || summary;
+      this.description = a.description;
+
       this.fields = [];
       if (this.description !== summary) {
-        this.fields.push({ name: '_ _', value: `${a.mission.faction} ${a.mission.type} on ${a.mission.node}` });
+        this.fields.push({ name: 'Mission', value: `${a.mission.faction} ${a.mission.type}`, inline: true });
+        this.fields.push({ name: 'Location', value: a.mission.node, inline: true });
       }
       this.fields.push({ name: 'Levels:', value: `${a.mission.minEnemyLevel} - ${a.mission.maxEnemyLevel}`, inline: true });
+
       this.fields.push({ name: 'Archwing Required', value: a.mission.archwingRequired ? 'Yes' : 'No', inline: true });
 
-      if (this.title.indexOf('cr') === -1) {
-        this.fields.push({ name: '_ _', value: `**Credits:** ${a.mission.reward.credits}cr`, inline: true });
+      if (this.title.indexOf('Cr') === -1) {
+        this.fields.push({ name: '_ _', value: `**Credits:** ${a.mission.reward.credits}`, inline: true });
       }
-      this.footer.text = `${a.eta} remaining`;
+      this.footer.text = `${a.eta} remaining | Expires `;
+      this.timestamp = a.expiry;
     }
   }
 }
 
 module.exports = AlertEmbed;
+
