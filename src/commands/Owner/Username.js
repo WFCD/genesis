7e28b1eb095ddeee469bd8858ecdd2512e9767c6
Tobiah <tobiah@protonmail.com>
2018-01-26 09:48:23 -0600

    Create Username.js

---
+++ src/commands/Owner/Username.js
@@ -0,0 +1,44 @@
+'use strict';
+
+const Command = require('../../Command.js');
+
+/**
+ * Sets the username for the bot
+ */
+class Username extends Command {
+  /**
+   * Constructs a callable command
+   * @param {Bot} bot The bot object
+   */
+  constructor(bot) {
+    super(bot, 'core.avatar', 'username', 'Set Bot avatar');
+    this.ownerOnly = true;
+    this.regex = new RegExp(`^${this.call}\\s*(.*)?`, 'i');
+    this.usages = [
+      {
+        description: 'Set the bot\'s username',
+        parameters: ['username'],
+      },
+    ];
+  }
+
+  /**
+   * Run the command
+   * @param {Message} message Message with a command to handle, reply to,
+   *                          or perform an action based on parameters.
+   * @returns {string} success status
+   */
+  async run(message) {
+    const username = message.strippedContent.match(this.regex)[1];
+    try {
+      await this.bot.client.user.username(username);
+      this.messageManager.reply(message, 'New Username set!', true, true);
+      return this.messageManager.statuses.SUCCESS;
+    } catch (e) {
+      this.logger.error(e);
+      return this.messageManager.statuses.FAILURE;
+    }
+  }
+}
+
+module.exports = Username;
