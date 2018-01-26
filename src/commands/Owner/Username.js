f057c2957c4756c6b0443e7ab183548a0d1ba419
Tobiah <tobiah@protonmail.com>
2018-01-26 09:50:30 -0600

    whoops, missed a few name spots

--- src/commands/Owner/Username.js
+++ src/commands/Owner/Username.js
@@ -11,7 +11,7 @@ class Username extends Command {
    * @param {Bot} bot The bot object
    */
   constructor(bot) {
-    super(bot, 'core.avatar', 'username', 'Set Bot avatar');
+    super(bot, 'core.username', 'username', 'Set Bot username');
     this.ownerOnly = true;
     this.regex = new RegExp(`^${this.call}\\s*(.*)?`, 'i');
     this.usages = [
