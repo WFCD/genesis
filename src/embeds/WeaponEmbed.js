472eb4457a64cba17f54ae30e9aa849136738fba
Tobiah <tobiah@protonmail.com>
2018-04-21 16:31:09 -0500

    riven dispo dots match game (white filled from the left)

--- src/embeds/WeaponEmbed.js
+++ src/embeds/WeaponEmbed.js
@@ -4,14 +4,14 @@ const BaseEmbed = require('./BaseEmbed.js');
 
 const { emojify } = require('../CommonFunctions.js');
 
-const dispositions = ['\\⚫\\⚫\\⚫\\⚫\\⚫', '\\⚫\\⚫\\⚫\\⚫\\⚪', '\\⚫\\⚫\\⚫\\⚪\\⚪', '\\⚫\\⚫\\⚪\\⚪\\⚪', '\\⚫\\⚪\\⚪\\⚪\\⚪', '\\⚪\\⚪\\⚪\\⚪\\⚪'];
+const dispositions = ['\\⚫\\⚫\\⚫\\⚫\\⚫', '\\⚪\\⚫\\⚫\\⚫\\⚫', '\\⚪\\⚪\\⚫\\⚫\\⚫', '\\⚪\\⚪\\⚪\\⚫\\⚫', '\\⚪\\⚪\\⚪\\⚪\\⚫', '\\⚪\\⚪\\⚪\\⚪\\⚪'];
 
 /**
  * Generates enemy embeds
  */
 class WeaponEmbed extends BaseEmbed {
   /**
-   * @param {Genesis} bot - An instance of Genesis
+   * @param {Genesis} bot - An instance of Genesisad
    * @param {Enhancement} weapon - The enhancement to send info on
    */
   constructor(bot, weapon) {
@@ -89,20 +89,20 @@ class WeaponEmbed extends BaseEmbed {
       if (weapon.secondary) {
         this.fields.push({
           name: 'Secondary Fire',
-          value: (weapon.secondary.trigger ? `**Trigger:** ${weapon.secondary.trigger || '-'}\n` : '' +
-          weapon.secondary.pellet ? `**Projectile:** ${weapon.secondary.pellet.name}\n` : '' +
-          weapon.secondary.rate ? `**Rate:** ${weapon.secondary.rate}\n` : '' +
-          weapon.secondary.flight ? `**Flight:**: ${weapon.secondary.flight}m\\s\n` : '' +
-          weapon.secondary.noise ? `**Noise:** ${weapon.secondary.noise}\n` : '' +
-          weapon.secondary.accuracy ? `**Accuracy:** ${weapon.secondary.accuracy}\n` : '' +
-          weapon.secondary.reload ? `**Reload:** ${weapon.secondary.reload}\n` : '' +
-          weapon.secondary.damage ? `**Damage:** ${emojify(weapon.secondary.damage || '-')}\n` : '' +
-          weapon.secondary.impact ? `**Impact:** ${weapon.secondary.impact}\n` : '' +
-          weapon.secondary.puncture ? `**Puncture:** ${weapon.secondary.puncture}\n` : '' +
-          weapon.secondary.slash ? `**Slash:** ${weapon.secondary.slash}\n` : '' +
-          weapon.secondary.crit_chance ? `**Critical Chance:** ${weapon.secondary.crit_chance}%\n` : '' +
-          weapon.secondary.crit_mult ? `**Critical Multiplier:** ${weapon.secondary.crit_mult}x\n` : '' +
-          weapon.secondary.status_chance ? `**Status Chance:** ${weapon.secondary.status_chance}%` : '') || '-',
+          value: (weapon.secondary.trigger ? `**Trigger:** ${weapon.secondary.trigger || '-'}\n` : `${
+            weapon.secondary.pellet}` ? `**Projectile:** ${weapon.secondary.pellet.name}\n` : `${
+              weapon.secondary.rate}` ? `**Rate:** ${weapon.secondary.rate}\n` : `${
+                weapon.secondary.flight}` ? `**Flight:**: ${weapon.secondary.flight}m\\s\n` : `${
+                  weapon.secondary.noise}` ? `**Noise:** ${weapon.secondary.noise}\n` : `${
+                    weapon.secondary.accuracy}` ? `**Accuracy:** ${weapon.secondary.accuracy}\n` : `${
+                      weapon.secondary.reload}` ? `**Reload:** ${weapon.secondary.reload}\n` : `${
+                        weapon.secondary.damage}` ? `**Damage:** ${emojify(weapon.secondary.damage || '-')}\n` : `${
+                          weapon.secondary.impact}` ? `**Impact:** ${weapon.secondary.impact}\n` : `${
+                            weapon.secondary.puncture}` ? `**Puncture:** ${weapon.secondary.puncture}\n` : `${
+                              weapon.secondary.slash}` ? `**Slash:** ${weapon.secondary.slash}\n` : `${
+                                weapon.secondary.crit_chance}` ? `**Critical Chance:** ${weapon.secondary.crit_chance}%\n` : `${
+                                  weapon.secondary.crit_mult}` ? `**Critical Multiplier:** ${weapon.secondary.crit_mult}x\n` : `${
+                                    weapon.secondary.status_chance}` ? `**Status Chance:** ${weapon.secondary.status_chance}%` : '') || '-',
           inline: true,
         });
       }
