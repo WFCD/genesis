68b0d9020d8f31db29171dcf67fba371d5dec580
Tobiah <tobiah@protonmail.com>
2018-08-08 15:39:11 -0500

    use context

--- src/commands/Worldstate/Simaris.js
+++ src/commands/Worldstate/Simaris.js
@@ -16,9 +16,9 @@ class Simaris extends Command {
     this.regex = new RegExp(`^${this.call}(?:\\s+on\\s+([pcsxb14]{2,3}))?$`, 'i');
   }
 
-  async run(message) {
+  async run(message, ctx) {
     const platformParam = message.strippedContent.match(this.regex)[1];
-    const platform = platformParam || await this.settings.getChannelSetting(message.channel, 'platform');
+    const platform = platformParam || ctx.platform;
     const ws = await this.bot.worldStates[platform.toLowerCase()].getData();
     const { simaris } = ws;
     await this.messageManager.embed(
