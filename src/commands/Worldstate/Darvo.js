728d9a85a7d7a633c3773998befc34e46f00006d
Tobiah <tobiah@protonmail.com>
2018-08-08 15:36:36 -0500

    use context

--- src/commands/Worldstate/Darvo.js
+++ src/commands/Worldstate/Darvo.js
@@ -16,9 +16,9 @@ class Darvo extends Command {
     this.regex = new RegExp(`^${this.call}(?:\\s+on\\s+([pcsxb14]{2,3}))?$`, 'i');
   }
 
-  async run(message) {
+  async run(message, ctx) {
     const platformParam = message.strippedContent.match(this.regex)[1];
-    const platform = platformParam || await this.settings.getChannelSetting(message.channel, 'platform');
+    const platform = platformParam || ctx.platform;
     const ws = await this.bot.worldStates[platform.toLowerCase()].getData();
     const deal = ws.dailyDeals[0];
     await this.messageManager.embed(message, new DarvoEmbed(this.bot, deal, platform), true, false);
