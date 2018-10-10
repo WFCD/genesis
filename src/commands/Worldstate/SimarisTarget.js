c3236aaa097a62cd9d889411a7cab317d2883127
esunder <esunder@gmail.com>
2018-10-09 19:05:16 -0500

    fix linting issues

--- src/commands/Worldstate/SimarisTarget.js
+++ src/commands/Worldstate/SimarisTarget.js
@@ -31,7 +31,6 @@ class SimarisTarget extends Command {
   }
 
   async run(message) {
-
     let query = message.strippedContent.match(this.regex)[1];
     this.logger.debug(`query: ${JSON.stringify(query)}`);
 
@@ -49,10 +48,10 @@ class SimarisTarget extends Command {
     const results = await request(options);
 
     // If there is a single result, show it
-    if (results.length == 1) {
-      this.messageManager.embed(message, new SynthesisTargetEmbed(this.bot, results, query), true, false);
-    }
-    else {
+    if (results.length === 1) {
+      this.messageManager.embed(message,
+        new SynthesisTargetEmbed(this.bot, results, query), true, false);
+    } else {
       // If there is more than one result, show a result list and the pages
       const pages = [];
       pages.push(new SynthesisTargetEmbed(this.bot, results, query));
@@ -65,7 +64,7 @@ class SimarisTarget extends Command {
       }
       if (parseInt(await this.settings.getChannelSetting(message.channel, 'delete_after_respond'), 10) && message.deletable) {
         message.delete(10000);
-      }  
+      }
     }
 
     return this.messageManager.statuses.SUCCESS;
--- src/embeds/SynthesisTargetEmbed.js
+++ src/embeds/SynthesisTargetEmbed.js
@@ -9,7 +9,7 @@ class SynthesisTargetEmbed extends BaseEmbed {
   /**
    * @param {Genesis} bot - An instance of Genesis
    * @param {Array.<SynthesisTarget>} synthTargets - The synthesis targets to send info on
-   * @param {String} query - The user's query
+   * @param {string} query - The user's query
    */
   constructor(bot, synthTargets, query) {
     super();
@@ -17,22 +17,17 @@ class SynthesisTargetEmbed extends BaseEmbed {
     this.thumbnail = {
       url: 'https://i.imgur.com/4Awre4E.png',
     };
-    if (synthTargets.length == 1) {
+    if (synthTargets.length === 1) {
       this.title = synthTargets[0].name;
-      // this.url = enhancement.info;
-      // this.thumbnail.url = enhancement.thumbnail;
-      // this.color = colors[enhancement.rarity.toLowerCase()];
-      this.fields = synthTargets[0].locations.map((loc) => {
-        return {
-          name: `${loc.mission}, ${loc.planet}`, 
-          value: `Faction: ${loc.faction}, Level: ${loc.level}, Spawn: ${loc.spawn_rate}`, 
-          inline: false,
-        }
-      });
+      this.fields = synthTargets[0].locations.map(loc => ({
+        name: `${loc.mission}, ${loc.planet}`,
+        value: `Faction: ${loc.faction}, Level: ${loc.level}, Spawn: ${loc.spawn_rate}`,
+        inline: false,
+      }));
     } else {
-      this.title = 'Multiple Targets matching "' + query + '"';
+      this.title = `Multiple targets matching "${query}"`;
       this.fields = [{ name: '\u200B', value: synthTargets.map(t => t.name).join('\n') }];
-      this.footer.text = 'Search through the results using the arrows below.'
+      this.footer.text = 'Search through the results using the arrows below.';
     }
   }
 }
