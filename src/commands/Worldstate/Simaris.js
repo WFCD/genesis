ee61320bd5c4cf796529358c41de4c463abf00ce
Erik Sunderland <esunder@gmail.com>
2018-10-11 11:50:15 -0500

    Synth command (#232)
    * saving progress of synth command
    
    * simaris target command
    
    * fix linting issues
    
    * simaris now handles the events and target searching
    
    * small bug with /help command
    
    * eslint fixes
    
    * remove debug logging
    
    * more linting

--- src/commands/Worldstate/Simaris.js
+++ src/commands/Worldstate/Simaris.js
@@ -1,7 +1,11 @@
 'use strict';
 
+const request = require('request-promise');
 const Command = require('../../models/Command.js');
 const SimarisEmbed = require('../../embeds/SimarisEmbed.js');
+const SynthesisTargetEmbed = require('../../embeds/SynthesisTargetEmbed.js');
+const { apiBase } = require('../../CommonFunctions');
+const { createPageCollector } = require('../../CommonFunctions');
 
 /**
  * Displays the current simaris target
@@ -13,11 +17,28 @@ class Simaris extends Command {
    */
   constructor(bot) {
     super(bot, 'warframe.worldstate.simaris', 'simaris', 'Display current Sanctuary status.');
-    this.regex = new RegExp(`^${this.call}(?:\\s+on\\s+([pcsxb14]{2,3}))?$`, 'i');
+    // Can only have 1 regex that a command can match on,
+    // so this is a combination of platform or target
+    this.regex = new RegExp(`(^${this.call}(?:\\s+on\\s+([pcsxb14]{2,3}))?$)|(^${this.call}(?:\\s+target\\s+([\\sa-zA-Z0-9]+))?$)`, 'i');
+    this.platformRegex = new RegExp(`^${this.call}(?:\\s+on\\s+([pcsxb14]{2,3}))?$`, 'i');
+    this.targetRegex = new RegExp(`^${this.call}(?:\\s+target\\s+([\\sa-zA-Z0-9]+))?$`, 'i');
   }
 
   async run(message, ctx) {
-    const platformParam = message.strippedContent.match(this.regex)[1];
+    const platformParam = await Simaris.getRegexParam(message, this.platformRegex);
+
+    const targetParam = await Simaris.getRegexParam(message, this.targetRegex);
+
+    if (targetParam !== undefined) {
+      await this.handleTargetCommand(message, targetParam);
+    } else {
+      await this.handleSimarisCommmand(message, ctx, platformParam);
+    }
+
+    return this.messageManager.statuses.SUCCESS;
+  }
+
+  async handleSimarisCommmand(message, ctx, platformParam) {
     const platform = platformParam || ctx.platform;
     const ws = await this.bot.worldStates[platform.toLowerCase()].getData();
     const { simaris } = ws;
@@ -25,7 +46,55 @@ class Simaris extends Command {
       message,
       new SimarisEmbed(this.bot, simaris, platform), true, false,
     );
-    return this.messageManager.statuses.SUCCESS;
+  }
+
+  async handleTargetCommand(message, targetParam) {
+    let query = targetParam;
+
+    const options = {
+      uri: `${apiBase}/synthtargets`,
+      json: true,
+      rejectUnauthorized: false,
+    };
+    if (query) {
+      query = query.trim().toLowerCase();
+    }
+
+    // Search the synth targets for the user's query
+    options.uri = `${apiBase}/synthtargets/search/${query}`;
+    const results = await request(options);
+
+    // If there is a single result, show it
+    if (results.length === 1) {
+      this.messageManager.embed(
+        message,
+        new SynthesisTargetEmbed(this.bot, results, query), true, false,
+      );
+    } else {
+      // If there is more than one result, show a result list and the pages
+      const pages = [];
+      pages.push(new SynthesisTargetEmbed(this.bot, results, query));
+      results.forEach((result) => {
+        pages.push(new SynthesisTargetEmbed(this.bot, [result], query));
+      });
+      if (pages.length) {
+        const msg = await this.messageManager.embed(message, pages[0], false, false);
+        await createPageCollector(msg, pages, message.author);
+      }
+      if (parseInt(await this.settings.getChannelSetting(message.channel, 'delete_after_respond'), 10) && message.deletable) {
+        message.delete(10000);
+      }
+    }
+  }
+
+  static getRegexParam(message, regex) {
+    let param;
+    const matchIndex = 1;
+    const matches = message.strippedContent.match(regex);
+    if (matches !== null) {
+      param = matches[matchIndex];
+    }
+    return param;
   }
 }
 
---
+++ src/embeds/SynthesisTargetEmbed.js
@@ -0,0 +1,35 @@
+'use strict';
+
+const BaseEmbed = require('./BaseEmbed.js');
+
+/**
+ * Generates synthesis target embeds
+ */
+class SynthesisTargetEmbed extends BaseEmbed {
+  /**
+   * @param {Genesis} bot - An instance of Genesis
+   * @param {Array.<SynthesisTarget>} synthTargets - The synthesis targets to send info on
+   * @param {string} query - The user's query
+   */
+  constructor(bot, synthTargets, query) {
+    super();
+
+    this.thumbnail = {
+      url: 'https://i.imgur.com/4Awre4E.png',
+    };
+    if (synthTargets.length === 1) {
+      this.title = synthTargets[0].name;
+      this.fields = synthTargets[0].locations.map(loc => ({
+        name: `${loc.mission}, ${loc.planet}`,
+        value: `Faction: ${loc.faction}, Level: ${loc.level}, Spawn: ${loc.spawn_rate}`,
+        inline: false,
+      }));
+    } else {
+      this.title = `Multiple targets matching "${query}"`;
+      this.fields = [{ name: '\u200B', value: synthTargets.map(t => t.name).join('\n') }];
+      this.footer.text = 'Search through the results using the arrows below.';
+    }
+  }
+}
+
+module.exports = SynthesisTargetEmbed;
