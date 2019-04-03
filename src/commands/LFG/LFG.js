590406f7645c9db0d9ea2bc0841afa3e7c4dc0f8
Tobiah <tobiah@protonmail.com>
2019-04-03 09:12:22 -0500

    fix: help triggering /h on lfg

--- src/commands/LFG/LFG.js
+++ src/commands/LFG/LFG.js
@@ -18,7 +18,7 @@ class AddLFG extends Command {
    */
   constructor(bot) {
     super(bot, 'lfg.add', 'lfg|h|lfm', 'Submit an LFG request.');
-    this.regex = new RegExp(`^(?:${this.call})\\s?(.+)?`, 'i');
+    this.regex = new RegExp(`^(?:${this.call})(?!elp)\\s?(.+)?`, 'i');
 
     this.usages = [
       {
