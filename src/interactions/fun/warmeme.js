38349fe8fbd6492447a61d776a5a4c09ef6c796d
Tobiah <tobiah@pm.me>
2021-08-06 00:27:48 -0500

    feat: reddit interactions closes #480

---
+++ src/interactions/fun/warmeme.js
@@ -0,0 +1,29 @@
+'use strict';
+
+const fetch = require('../../resources/Fetcher');
+const Embed = require('../../embeds/BaseEmbed');
+
+module.exports = class Reddit extends require('../../models/Interaction') {
+  static enabled = false;
+  static subreddit = 'memeframe'
+  static command = {
+    name: 'memeframe',
+    description: 'Get a Warframe meme',
+  };
+
+  static async commandHandler(interaction) {
+    await interaction.defer();
+    const base = `https://reddit.com/r/${Reddit.subreddit}/random/.json`;
+    const {
+      permalink, url, title, subreddit_name_prefixed: srn, created_utc: ts,
+    } = (await fetch(base))[0].data.children[0].data;
+    const embed = new Embed();
+    embed.setTitle(title);
+    embed.setURL(`https://reddit.com${permalink}`);
+    embed.setImage(url);
+    embed.setFooter(`${srn} • Posted`, 'https://www.redditinc.com/assets/images/site/reddit-logo.png');
+    embed.setTimestamp(ts * 1000);
+
+    await interaction.editReply({ embeds: [embed] });
+  }
+};
