6f6d21fca328462b6b4838d4abe42afeb0b0f3a5
Tobiah <tobiah@pm.me>
2022-03-18 21:02:29 -0500

    more i18n

--- src/embeds/SalesEmbed.js
+++ src/embeds/SalesEmbed.js
@@ -8,28 +8,26 @@ const darvo = `${assetBase}/img/darvo-md.png`;
 /**
  * Generates daily deal embeds
  */
-class SalesEmbed extends BaseEmbed {
+module.exports = class SalesEmbed extends BaseEmbed {
   /**
-   * @param {Genesis} bot - An instance of Genesis
    * @param {Array.<FeaturedItemSales>} sales - The sales to be displayed as featured or popular
    * @param {string} platform - platform
+   * @param {I18n} i18n internationalizer
    */
-  constructor(bot, sales, platform) {
+  constructor(sales, { platform, i18n }) {
     super();
 
     this.color = 0x0000ff;
-    this.title = sales[0].isPopular ? `[${platform.toUpperCase()}] Popular Sales ` : `[${platform.toUpperCase()}] Featured Deal`;
+    this.title = sales[0].isPopular ? i18n`[${platform.toUpperCase()}] Popular Sales` : i18n`[${platform.toUpperCase()}] Featured Deal`;
     this.thumbnail = {
       url: darvo,
     };
     this.fields = [];
     sales.forEach((sale) => {
       this.fields.push({
-        name: `${sale.item}, ${sale.premiumOverride}p ${sale.discount > 0 ? `${sale.discount}% off` : ''}`,
-        value: `Expires in ${sale.eta}`,
+        name: i18n`${sale.item}, ${sale.premiumOverride}p ${sale.discount > 0 ? `${sale.discount}% off` : ''}`,
+        value: i18n`Expires in ${sale.eta}`,
       });
     });
   }
-}
-
-module.exports = SalesEmbed;
+};
