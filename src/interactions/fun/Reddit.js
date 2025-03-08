8c40fc6211c5fc7737268b26c30823daa0a994d9
Matej Voboril <tobiah@pm.me>
2025-03-08 16:10:49 -0600

    fix(#937): random reddit post update

--- src/interactions/fun/Reddit.js
+++ src/interactions/fun/Reddit.js
@@ -1,4 +1,4 @@
-import fetch from 'node-fetch';
+import { getPost } from 'random-reddit';
 
 import Embed from '../../embeds/BaseEmbed.js';
 import Interaction from '../../models/Interaction.js';
@@ -13,21 +13,21 @@ export default class Reddit extends Interaction {
 
   static async commandHandler(interaction, ctx) {
     await interaction.deferReply({ ephemeral: ctx.ephemerate });
-    const base = `https://reddit.com/r/${this.subreddit}/random/.json`;
-    const {
-      permalink,
-      url,
-      title,
-      subreddit_name_prefixed: srn,
-      created_utc: ts,
-    } = (await fetch(base).then((d) => d.json()))[0].data.children[0].data;
+    const post = await getPost(this.subreddit);
+    const { selftext, permalink, thumbnail: url, title, subreddit_name_prefixed: srn, created_utc: ts } = post;
     const embed = new Embed();
-    embed.setTitle(title);
+    embed.setDescription(selftext ?? '');
+    embed.setTitle(title.replaceAll('&amp;', '&'));
     embed.setURL(`https://reddit.com${permalink}`);
     embed.setImage(url);
+
+    if (post.is_gallery) {
+      embed.setImage(post.media_metadata[post.gallery_data.items[0].media_id].s.u.replaceAll('&amp;', '&'));
+    } else {
+      embed.setImage(post.preview.images[0].source.url.replace('&amp;', '&'));
+    }
     embed.setFooter({
       text: `${srn} • Posted`,
-      iconURL: 'https://www.redditinc.com/assets/images/site/reddit-logo.png',
     });
     embed.setTimestamp(ts * 1000);
 
